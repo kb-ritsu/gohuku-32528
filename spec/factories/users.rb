93e520ab9864186d2a9a114174a0c0d04db41585
kb-ritsu <microyasuki@hotmail.co.jp>
2021-02-03 21:16:32 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    email                 {Faker::Internet.free_email}
     first_name            {Faker::Name.first_name}
     last_name             {Faker::Name.last_name}
     first_name_katakana   {"タロウ"}
