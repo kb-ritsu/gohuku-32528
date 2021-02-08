22ae31e414ab2b7669f9d2b97aa0c201c0659f48
kb-ritsu <microyasuki@hotmail.co.jp>
2021-02-08 23:10:11 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name,:id])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name,:password])
   end
 end
